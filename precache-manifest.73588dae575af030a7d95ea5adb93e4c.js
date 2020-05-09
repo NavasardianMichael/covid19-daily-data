917523210939246feedb5ec78862b576dacc3d58
Navasardian Michael <navasardianmichael@gmail.com>
2020-05-09 18:42:26 +0400

    Updates

---
+++ precache-manifest.73588dae575af030a7d95ea5adb93e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f67a3dd192ed4d0f893abb49f58adc",
+    "url": "/covid19-daily-data/index.html"
+  },
+  {
+    "revision": "bf1820c050aab9a623f4",
+    "url": "/covid19-daily-data/static/js/2.5abfe451.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-daily-data/static/js/2.5abfe451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412e89b328346035422c",
+    "url": "/covid19-daily-data/static/js/main.3365f050.chunk.js"
+  },
+  {
+    "revision": "14e270ff3b64426f2e08",
+    "url": "/covid19-daily-data/static/js/runtime-main.b5f9fd44.js"
+  }
+]);
